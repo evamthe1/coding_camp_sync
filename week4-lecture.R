ac4ac8934bb8cfa8c3b6537c99d2b285fbb92a96
jiaoy-12371081 <117399218+jiaoy-12371081@users.noreply.github.com>
2022-11-03 16:01:39 -0500

    Add files via upload

---
+++ week4-lecture.R
@@ -0,0 +1,55 @@
+
+### PPHA 30110: Coding Lab for Public Policy
+### Week 4
+### Instructor: Arthur Cheib
+
+
+### Try it yourself 13- slide 17 #  ##
+
+# (a) Import the dataset containing school's attendance (metrics_attendance.xlsx) of every public school in Chicago
+
+# (b) Make the data look somewhat tidy!
+# Hint: We need to have a column named YEAR
+
+
+### Try it yourself 14 - slide 22
+
+# (a) Import the lakers_game.csv dataset to your current R session
+# This data is about Los Angeles Lakers 2008-2009 season
+
+# (b) Separate the 'date' column into month, day and year columns.
+
+# (c) Discover how many games the Lakers played per month for that season
+# Hint: group_by() and summarize() function could help here / or the count() function
+
+### Try it yourself 15 - slide 30 ###
+
+# (a) Import the gapminder.csv dataset to your current R session
+
+# (b) Create a new dataframe from gapminder that only contains information about 1952.
+
+# (c) Transform the continent colum of the dataframe from (b) into a character type
+
+# (d) Extract the continent column of this new object/dataframe as a dataframe
+
+# (e) Extract the continent column of this new object/dataframe as a vector. Save this a new object
+
+# (f) Get only the first 35 elements of this vector
+
+# (g) Using the vector from (e) get only unique values
+
+
+### Try it yourself 16 - slide 37 ###
+
+# (a) Using the gapminder dataset, cx`reate a dummy variable (takes on 0 or 1 only),
+# depending on the gdpPercap column being bigger than 15,000. if TRUE = 1; not = 0
+
+
+### Try it yourself 17 - slide ###
+
+# (a) Create two numeric variables (chose numbers that could be scores of a soccer game):
+# each one named after a team in the World Cup
+
+# (b) Create a if-statement evaluates which variable is higher and then
+# prints who won the World Cup depending on which numeric variable
+
